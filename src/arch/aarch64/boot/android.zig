6e4eb693c33028203b85895d0eae93f5253fa68c
zshzebra <ryder@retzlaff.family>
2026-06-17 21:39:14 +1000

    feat: Experiment, write bss with FF's

--- src/arch/aarch64/boot/android.zig
+++ src/arch/aarch64/boot/android.zig
@@ -14,9 +14,20 @@ export fn _start() linksection(".text.boot") callconv(.naked) noreturn {
         \\ adrp x1, __stack_top
         \\ add  x1, x1, :lo12:__stack_top
         \\ mov  sp, x1
-        \\ bl   main
-        \\2: wfe
+        \\ adrp x9, __bss_start
+        \\ add x9, x9, :lo12:__bss_start
+        \\ adrp x10, __bss_end
+        \\ add x10, x10, :lo12:__bss_end
+        \\ mov x8, #-1
+        \\2:
+        \\ cmp x9,x10
+        \\ b.hs 3f
+        \\ str x8, [x9], #8
         \\ b 2b
+        \\3:
+        \\ bl   main
+        \\4: wfe
+        \\ b 4b
     );
 }
 
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
 
+var should_be_zero: u32 = 0;
+
 // TODO: Some real payload
 pub fn kernel_main() void {
     const fb: [*]volatile u32 = @ptrFromInt(0x9c400000);
-    @memset(fb[0 .. 1080 * 2280], 0xffff0000);
+    @memset(fb[0 .. 1080 * 2280], 0xffa6e3a1);
+
+    if (@as(*volatile u32, &should_be_zero).* != 0) @memset(fb[0 .. 1080 * 2280], 0xffff0000);
 }
